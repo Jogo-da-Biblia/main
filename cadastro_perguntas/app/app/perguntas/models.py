8c0707f669703b61c47d7567df34857e9cdac828
Jefferson472 <jefferson472@hotmail.com>
2022-05-11 07:44:52 -0300

    fix model perguntas

--- cadastro_perguntas/app/app/perguntas/models.py
+++ cadastro_perguntas/app/app/perguntas/models.py
@@ -1,5 +1,3 @@
-from tabnanny import verbose
-
 from app.biblia.models import Livro, Versiculo
 from app.core.models import User
 from django.db import models
@@ -8,6 +6,9 @@ from django.db import models
 class Tema(models.Model):
     nome = models.CharField(max_length=50)
     cor = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.nome
 
     class Meta:
         db_table = 'tema'
@@ -23,21 +24,26 @@ class Pergunta(models.Model):
     ]
 
     id = models.AutoField(primary_key=True)
-    grupo = models.ForeignKey(Grupo, related_name='grupo', on_delete=models.CASCADE)
+    grupo = models.ForeignKey(
+        'Grupo', related_name='grupo', on_delete=models.CASCADE)
     tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
     enunciado = models.TextField()
     tipo_resposta = models.CharField(
         max_length=3, choices=TIPO_RESPOSTA, verbose_name='Tipo de Resposta')
-    refencia_resposta = models.ForeignKey(Referencia, related_name='refencia_resposta', on_delete=models.CASCADE)
-    outras_referencias = models.ForeignKey(Referencia, related_name='outras_referencias', on_delete=models.CASCADE)
-    criado_por = models.ForeignKey(User, related_name='criado_por',
-                                   on_delete=models.CASCADE)
+    refencia_resposta = models.ForeignKey(
+        'Referencia', on_delete=models.CASCADE,
+        related_name='referencia_resposta')
+    outras_referencias = models.ForeignKey(
+        'Referencia', on_delete=models.CASCADE)
+    status = models.BooleanField(default=True, verbose_name='Pergunta Status')
+    criado_por = models.ForeignKey(
+        User, related_name='criado_por', on_delete=models.CASCADE)
     criado_em = models.DateTimeField(auto_now_add=True)
-    revisado_por = models.ForeignKey(User, related_name='revisado_por',
-                                     on_delete=models.CASCADE)
+    revisado_por = models.ForeignKey(
+        User, related_name='revisado_por', on_delete=models.CASCADE)
     revisado_em = models.DateTimeField(auto_now_add=True)
-    publicado_por = models.ForeignKey(User, related_name='publicado_por',
-                                      on_delete=models.CASCADE)
+    publicado_por = models.ForeignKey(
+        User, related_name='publicado_por', on_delete=models.CASCADE)
     publicado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
 
@@ -53,7 +59,7 @@ class Referencia(models.Model):
     id = models.AutoField(primary_key=True)
     livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
     versiculo = models.ForeignKey(Versiculo, on_delete=models.CASCADE,
-        verbose_name='Versículo')
+                                  verbose_name='Versículo')
 
     def __str__(self):
         return f"{self.livro} {self.versiculo}"
@@ -65,8 +71,9 @@ class Referencia(models.Model):
 class Alternativa(models.Model):
     texto = models.TextField()
     pergunta = models.ForeignKey(Pergunta, related_name='alternativas',
-                                        on_delete=models.CASCADE)
-    resposta_certa = models.BooleanField(default=False) # Marca se essa é uma das respostas corretas ou é uma falsa
+                                 on_delete=models.CASCADE)
+    # Marca se essa é uma das respostas corretas ou é uma falsa
+    resposta_certa = models.BooleanField(default=False)
 
     def __str__(self):
         return self.texto
@@ -97,7 +104,7 @@ class Grupo(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
     class Meta:
         db_table = 'grupo'
         verbose_name = 'Grupo'
