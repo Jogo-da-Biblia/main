2cd4ba9f364fce1a3175a63a6c168f8b101d8977
Jefferson472 <jefferson472@hotmail.com>
2022-05-15 09:55:30 -0300

    mv model comentario from pergunta to comentario

--- cadastro_perguntas/app/app/perguntas/models.py
+++ cadastro_perguntas/app/app/perguntas/models.py
@@ -82,19 +82,3 @@ class Alternativa(models.Model):
 
     class Meta:
         db_table = 'alternativa'
-
-
-class Comentario(models.Model):
-    id = models.AutoField(primary_key=True)
-    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
-    email = models.CharField(max_length=126)
-    phone = models.CharField(max_length=11)
-    is_whatsapp = models.BooleanField('É Whatsapp?', default=True)
-    mensagem = models.TextField()
-    criado_em = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.mensagem} - {self.email}'
-
-    class Meta:
-        db_table = 'comentario'
