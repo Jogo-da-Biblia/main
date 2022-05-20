5ed490081fbd144c55a5ff23b439460322d2d0dc
Jefferson Miranda <43066724+Jefferson472@users.noreply.github.com>
2022-05-20 11:54:34 -0300

    fix secret keys database to .env

--- cadastro_perguntas/app/app/settings.py
+++ cadastro_perguntas/app/app/settings.py
@@ -94,15 +94,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
